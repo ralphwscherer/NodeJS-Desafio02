dc5ad7062b77b6d18acf88b31258c4ac83a05952
Ralph Wosgrau Scherer <ralphwscherer@live.com>
2022-06-30 18:58:12 -0300

    Finalizado o desafio #2 do curso de NodeJS

--- src/index.js
+++ src/index.js
@@ -10,19 +10,71 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' })
+  }
+
+  request.user = user
+
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  const amountTodos = user.todos.length
+
+  if (amountTodos >= 10 && !user.pro) {
+    return response.status(403).json({ error: 'User is not eligible!' })
+  }
+  
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' })
+  }
+
+  const idIsValid = validate(id)
+
+  if (!idIsValid) {
+    return response.status(400).json({ error: 'Id not is valid!' })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found!' })
+  }
+
+  request.user = user
+  request.todo = todo
+
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' })
+  }
+
+  request.user = user
+
+  next()
 }
 
 app.post('/users', (request, response) => {
